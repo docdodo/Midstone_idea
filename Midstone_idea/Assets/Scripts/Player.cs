b596fff4128d5b16adb4258fdf1e93ccbbca08c0
Robert <robert.blank.wardo@gmail.com>
2019-01-14 13:18:32 -0500

    Starting project
    nothing coded yet

---
+++ Midstone_idea/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float movespeed;
+	
+	void Start () {
+        movespeed = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+       
+	}
+}
