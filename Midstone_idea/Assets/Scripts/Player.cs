321d5106658fcbfa608101ba5d1b4f1670d4b38e
Robert <robert.blank.wardo@gmail.com>
2019-01-16 16:18:09 -0500

    working on lightning attacks
    changed some player code around

--- Midstone_idea/Assets/Scripts/Player.cs
+++ Midstone_idea/Assets/Scripts/Player.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float movespeed;
-	
-	void Start () {
+    public float maxhp;
+    public float hp;
+    public float Mp;
+    public float MaxMp;
+    public GameObject LightningHbox;
+    void Start () {
         movespeed = 5.0f;
+        MaxMp = 100.0f;
+        Mp = MaxMp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.DrawRay(transform.position, transform.forward);
+        transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        if (Input.GetButton("Lightning") & Mp > 0.0f)
+        {
+
+            LightningHbox.SetActive(true);
+            Mp = Mp - 0.1f;
+
+        }
+        if (Input.GetButton("Lightning") & Mp <= 0.0f)
+        {
+            LightningHbox.SetActive(false);
+            Debug.Log("out of mana");
+        }
+
+
+    }
+ 
+
+
+
+
+
+
+
+
+
+    IEnumerator Charging1()
+    {
         
-            transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
-       
-	}
+            
+            yield return new WaitForSeconds(0.5f);
+            LightningHbox.SetActive(true);
+        Mp = Mp - 1;
+        
+            
+        
+
+    }
+
+
+
+
+
 }
