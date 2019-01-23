b79b948ab1f28f6a6c08bbd748f4466a95133954
Diego <dg21@users.noreply.github.com>
2019-01-23 17:39:05 -0500

    Test
    syncing the hubs

---
+++ Midstone_idea/Assets/Scripts/Spawner Scipt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScipt : MonoBehaviour {
+    public Transform spawnPos;
+    public Transform spawnie;
+	
+	
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            Instantiate(spawnie, spawnPos.position, spawnPos.rotation);
+        }
+
+	}
+}
