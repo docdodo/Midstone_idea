2e46779c98d2bcc52313b8ceb8b97dfe41557928
Diego <dg21@users.noreply.github.com>
2019-01-24 18:32:36 -0500

    update with a simple sTimedSpawner

--- Midstone_idea/Assets/Scripts/Player.cs
+++ Midstone_idea/Assets/Scripts/Player.cs
@@ -8,15 +8,21 @@ public class Player : MonoBehaviour {
     public float hp;
     public float Mp;
     public float MaxMp;
+     
     public GameObject LightningHbox;
+
+   
+
     void Start () {
         movespeed = 5.0f;
         MaxMp = 100.0f;
         Mp = MaxMp;
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    void Update()
+    {
         Debug.DrawRay(transform.position, transform.forward);
         transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
         if (Input.GetButton("Lightning") & Mp > 0.0f)
@@ -26,26 +32,25 @@ public class Player : MonoBehaviour {
             Mp = Mp - 0.1f;
 
         }
-        if(Input.GetButtonUp("Lightning"))
+        if (Input.GetButtonUp("Lightning"))
         {
             LightningHbox.SetActive(false);
         }
-        
+
         if (Input.GetButton("Lightning") & Mp <= 0.0f)
         {
             LightningHbox.SetActive(false);
             Debug.Log("out of mana");
         }
 
+                          
 
     }
- 
 
+      
+       
 
-
-
-
-
+   
 
 
 
